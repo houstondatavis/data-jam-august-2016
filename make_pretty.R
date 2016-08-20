b32bced3accdfe19df091e6f548bf547e9f8b9e5
iota <iota@Iota>
2016-08-20 00:34:47 -0500

    Add make_pretty.R
    A script to convert column names to friendlier ones

---
+++ make_pretty.R
@@ -0,0 +1,7 @@
+df <- read.csv('csv/county_facts.csv', header=T, stringsAsFactors=F)
+pretty_names <- read.csv('pretty_names.csv', header=T, stringsAsFactors=F)
+names(df) <- c("fips", "area_name", "state_abbreviation", pretty_names$pretty_name)
+
+write.csv(df, 'csv/county_facts.csv')
+feather::write_feather(df, 'feather/county_facts.feather')
+
