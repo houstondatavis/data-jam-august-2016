1d76d810f08a2305ca43b32d5da7861e029a88cb
Neeraj Tandon <neerajtandon25@gmail.com>
2016-08-20 00:46:36 -0500

    Fix make_pretty.R
    Suppress row.names from being written to pretty csv

--- make_pretty.R
+++ make_pretty.R
@@ -2,6 +2,6 @@ df <- read.csv('csv/county_facts.csv', header=T, stringsAsFactors=F)
 pretty_names <- read.csv('pretty_names.csv', header=T, stringsAsFactors=F)
 names(df) <- c("fips", "area_name", "state_abbreviation", pretty_names$pretty_name)
 
-write.csv(df, 'csv/county_facts.csv')
+write.csv(df, 'csv/county_facts.csv', row.names=F)
 feather::write_feather(df, 'feather/county_facts.feather')
 
