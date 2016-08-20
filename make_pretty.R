8fadaad3f9535465c0ccdd474f64de90a0fce361
Neeraj Tandon <neerajtandon25@gmail.com>
2016-08-20 01:03:42 -0500

    Update make_pretty.R
    Write updated data dictionaries in feather and csv

--- make_pretty.R
+++ make_pretty.R
@@ -1,7 +1,12 @@
 df <- read.csv('csv/county_facts.csv', header=T, stringsAsFactors=F)
 pretty_names <- read.csv('pretty_names.csv', header=T, stringsAsFactors=F)
-names(df) <- c("fips", "area_name", "state_abbreviation", pretty_names$pretty_name)
 
+names(df) <- c("fips", "area_name", "state_abbreviation", pretty_names$pretty_name)
+data_dict <- pretty_names[, c("pretty_name", "description")]
+
+write.csv(data_dict, 'csv/county_facts_dictionary.csv', row.names=F)
 write.csv(df, 'csv/county_facts.csv', row.names=F)
+
+feather::write_feather(data_dict, 'feather/county_facts_dictionary.feather')
 feather::write_feather(df, 'feather/county_facts.feather')
 
